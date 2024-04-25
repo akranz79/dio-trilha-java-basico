cd47553b3b8855c58083d022b89ad7501109b06f
Alexandre Kranz <ahkranz79@gmail.com>
2024-04-25 19:33:26 -0300

    refactor: ContaTerminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,36 +1,37 @@
 import java.util.Scanner;
+
 public class ContaTerminal {
     private int numeroCC;
     private String agencia;
     private String nomeCliente;
     private double saldoCC;
 
-    
     public static void main(String[] args) {
-
-        //TODO: Conhecer e importar a classe Scanner
-        Scanner scanner = new Scanner(System.in);
         ContaTerminal conta = new ContaTerminal();
-        
+        conta.lerDadosConta();
+        conta.exibirMensagemContaCriada();
+    }
+
+    private void lerDadosConta() {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.print("Por favor, digite o seu nome: ");
-        conta.nomeCliente = scanner.nextLine();
-        
+        this.nomeCliente = scanner.nextLine();
+
         System.out.print("Por favor, digite o numero da Agencia: ");
-        conta.agencia = scanner.nextLine();
+        this.agencia = scanner.nextLine();
 
         System.out.print("Por favor, digite o numero da Conta: ");
-        conta.numeroCC = scanner.nextInt();
+        this.numeroCC = scanner.nextInt();
 
         System.out.print("Por favor, digite o seu Saldo: ");
-        conta.saldoCC = scanner.nextDouble();
-
-        //Exibir as mensagens para o usuário
-
-        //Obter pela scanner os valores digitados no terminal
-
-        //Exibir a mensagem conta criada
-        System.out.println("Olá "+conta.nomeCliente+" , obrigado por criar uma conta em nosso banco, sua agencia é " + conta.agencia + ", conta "+conta.numeroCC+" e seu saldo R$ "+conta.saldoCC+" já está disponível para saque.");
+        this.saldoCC = scanner.nextDouble();
 
         scanner.close();
+    }
+
+    private void exibirMensagemContaCriada() {
+        System.out.println("Olá " + this.nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agencia é " 
+                            + this.agencia + ", conta " + this.numeroCC + " e seu saldo R$ " + this.saldoCC + " já está disponível para saque.");
     }
 }

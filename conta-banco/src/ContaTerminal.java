95532a8c354de4588d2ee82991c09ec6913f63f8
Alexandre Kranz <ahkranz79@gmail.com>
2024-04-25 14:57:04 -0300

    meu projeto conta-banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,15 @@
 public class ContaTerminal {
     
     public static void main(String[] args) {
+
+        //TODO: Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para o usuário
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
+        
         System.out.println("Conta terminal criada com sucesso!");
     }
     
